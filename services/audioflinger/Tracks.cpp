b0dbce0e6811b89c739c29779b8a08ce9bfaa522
Eric Laurent <elaurent@google.com>
2025-07-01 14:57:10 +0000

    AudioFlinger: more duplicating mixer fixes
    Additional fixes to support buffer durations of less than
    10ms on the Bluetooth software encoder path:
    - Fix ClientProxy implementation to allow for less than
    10ms timeouts in obtainBuffer()
    - Dynamically adjust the wait time in OutputTrack::write()
    to skip waiting if one of the output tracks is near underruning
    
    Bug: 412835975
    Test: repor steps in bug
    Flag: EXEMPT bug fix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:8b4bb667fe06b1362f4228eb7cdc6b7df0153d5c)
    Merged-In: I82a471504258a4bff9cd10df1bae18515c2bb746
    Change-Id: I82a471504258a4bff9cd10df1bae18515c2bb746
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/audioflinger/Tracks.cpp
+++ services/audioflinger/Tracks.cpp
@@ -2378,7 +2378,8 @@ OutputTrack::OutputTrack(
               nullptr /* buffer */, (size_t)0 /* bufferSize */, nullptr /* sharedBuffer */,
               AUDIO_SESSION_NONE, getpid(), attributionSource, AUDIO_OUTPUT_FLAG_NONE,
               TYPE_OUTPUT, AUDIO_PORT_HANDLE_NONE,
-              /*frameCountToBeReady*/ playbackThread->frameCount()),
+              /*frameCountToBeReady*/ sourceFramesNeededWithTimestretch(sampleRate,
+                      playbackThread->frameCount(), playbackThread->sampleRate(), 1.0f /*speed*/)),
     mActive(false), mSourceThread(sourceThread)
 {
     if (mCblk != NULL) {
@@ -2395,6 +2396,8 @@ OutputTrack::OutputTrack(
         mClientProxy->setVolumeLR(GAIN_MINIFLOAT_PACKED_UNITY);
         mClientProxy->setSendLevel(0.0);
         mClientProxy->setSampleRate(sampleRate);
+        uint32_t waitTimeMs = (playbackThread->frameCount() * 1000) / playbackThread->sampleRate();
+        mClientProxy->setMinMeasureMs(waitTimeMs);
     } else {
         ALOGW("%s(%d): Error creating output track on thread %d",
                 __func__, mId, (int)mThreadIoHandle);
@@ -2474,7 +2477,7 @@ ssize_t OutputTrack::write(void* data, uint32_t frames)
     inBuffer.frameCount = frames;
     inBuffer.raw = data;
     uint32_t waitTimeLeftMs = mSourceThread->waitTimeMs();
-    while (waitTimeLeftMs) {
+    while (true) {
         // First write pending buffers, then new data
         if (mBufferQueue.size()) {
             pInBuffer = mBufferQueue.itemAt(0);
@@ -2534,6 +2537,9 @@ ssize_t OutputTrack::write(void* data, uint32_t frames)
             } else {
                 break;
             }
+        }
+        if (waitTimeLeftMs == 0) {
+            break;
         }
     }
 
